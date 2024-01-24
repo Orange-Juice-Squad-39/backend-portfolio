e2a53305ba2770c722045ab2db04eaff46ac648f
petry <petry@pop-os.localdomain>
2024-01-24 14:41:32 -0300

    RFB005: Rota para / desenvolvida

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { ProjectsModule } from './modules/projects/projects.module';
+import { AppController} from './app.controller';
 
 @Module({
   imports: [UsersModule, ProjectsModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
