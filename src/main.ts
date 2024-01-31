04762f30614b7a5d2a75eb707b335c039a8afcd5
Everton Dias <55013045+Eivrens@users.noreply.github.com>
2024-01-31 14:30:27 -0300

    Update main.ts
    Add port 4444

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function main() {
-  const port: number = 3000;
+  const port: number = 4444;
 
   const app = await NestFactory.create(AppModule, { cors: true });
 
